f1397f02629026378589815a570dcc34b6dda5ea
musteey <mustaphasunusi5@gmail.com>
2024-08-08 14:19:00 +0100

    fixed some animation issues

--- src/components/mediaSharing.vue
+++ src/components/mediaSharing.vue
@@ -5,10 +5,10 @@
         <img src="@/assets/phones.png" alt="phones" data-aos="zoom-in" data-aos-duration="1000" class="w-full h-full" />
       
     </section>
-    <section class="flex-1 px-5 md:px-0"  data-aos="zoom-in">
+    <section class="flex-1 px-5 md:px-0"  >
         <div v-for="media of mediaContent" class="flex flex-col gap-5 mb-10">
-            <h2 class="md:text-4xl text-3xl font-bold font-heading">{{ media.title }}</h2>
-            <p class="text-base font-normal">{{ media.Content }}</p>
+            <h2 class="md:text-4xl text-3xl font-bold font-heading" data-aos="fade-up">{{ media.title }}</h2>
+            <p class="text-base font-normal" data-aos="fade-up" data-aos-delay="100">{{ media.Content }}</p>
         </div>
     </section> 
   </div>
